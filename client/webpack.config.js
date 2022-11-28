d539b94a37dad62ce88439ec122a6913e2f12805
bburton5 <bburton5@uw.edu>
2022-11-27 22:53:49 -0800

    added scope to manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -35,6 +35,7 @@ module.exports = () => {
         background_color: "#7eb4e2",
         theme_color: "#7eb4e2",
         start_url: "./",
+        scope: "./",
         publicPath: "./",
         icons: [
           {
