41c91e9af18c1eba491cdeab1ea50a1e5b01794d
bburton5 <bburton5@uw.edu>
2022-11-27 23:25:28 -0800

    debugging idb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,19 +16,14 @@ const initdb = async () =>
 export const putDb = async (content) => {
   console.log("Post to the database");
 
-  // Create a connection to the database database and version we want to use.
   const jatesDb = await openDB("jate", 1);
 
-  // Create a new transaction and specify the database and data privileges.
   const tx = jatesDb.transaction("jate", "readwrite");
 
-  // Open up the desired object store.
   const store = tx.objectStore("jate");
 
-  // Use the .add() method on the store and pass in the content.
   const request = store.add({ todo: content });
 
-  // Get confirmation of the request.
   const result = await request;
   console.log("🚀 - data saved to the database", result);
 };
@@ -37,19 +32,14 @@ export const putDb = async (content) => {
 export const getDb = async () => {
   console.log("GET all from the database");
 
-  // Create a connection to the database database and version we want to use.
   const jatesDb = await openDB("jate", 1);
 
-  // Create a new transaction and specify the database and data privileges.
   const tx = jatesDb.transaction("jate", "readonly");
 
-  // Open up the desired object store.
   const store = tx.objectStore("jate");
 
-  // Use the .getAll() method to get all data in the database.
   const request = store.getAll();
 
-  // Get confirmation of the request.
   const result = await request;
   console.log("result.value", result);
   return result;
